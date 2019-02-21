355d9a2d6c0d40d35b7904bf341f454114b45f73
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-02-21 20:09:19 -0300

    Add tests for GeoStats.jl API

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,35 @@
+using GeoStatsBase
+using GeoStatsDevTools
 using TuringPatterns
-using Base.Test
+using Plots; gr()
+using VisualRegressionTests
+using Test, Pkg, Random
 
-# write your own tests here
-@test 1 == 2
+# environment settings
+islinux = Sys.islinux()
+istravis = "TRAVIS" ∈ keys(ENV)
+datadir = joinpath(@__DIR__,"data")
+visualtests = !istravis || (istravis && islinux)
+if !istravis
+  Pkg.add("Gtk")
+  using Gtk
+end
+
+@testset "TuringPatterns.jl" begin
+  @testset "Basic usage" begin
+    # TODO:
+  end
+
+  @testset "GeoStats.jl API" begin
+    problem = SimulationProblem(RegularGrid{Float64}(200,200), :z => Float64, 3)
+
+    solver = TuringPat()
+
+    Random.seed!(2019)
+    solution = solve(problem, solver)
+
+    if visualtests
+      @plottest plot(solution,size=(1000,300)) joinpath(datadir,"GeoStatsAPI.png") !istravis
+    end
+  end
+end
