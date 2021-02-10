6d73e6a4b2d31dc8752b4c2f7f4fe908ff7f13e8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-02-10 13:43:41 -0300

    Move to ReferenceTests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using TuringPatterns
 using GeoStatsBase
-using Plots, VisualRegressionTests
+using Plots; gr(size=(600,400))
+using ReferenceTests, ImageIO
 using Test, Random
 
 # workaround GR warnings
@@ -11,6 +12,19 @@ isCI = "CI" ∈ keys(ENV)
 islinux = Sys.islinux()
 visualtests = !isCI || (isCI && islinux)
 datadir = joinpath(@__DIR__,"data")
+
+# helper functions for visual regression tests
+function asimage(plt)
+  io = IOBuffer()
+  show(io, "image/png", plt)
+  seekstart(io)
+  ImageIO.load(io)
+end
+macro test_ref_plot(fname, plt)
+  esc(quote
+    @test_reference $fname asimage($plt)
+  end)
+end
 
 @testset "TuringPatterns.jl" begin
   @testset "Basic usage" begin
@@ -23,7 +37,7 @@ datadir = joinpath(@__DIR__,"data")
     solution = solve(problem, TPS())
 
     if visualtests
-      @plottest plot(solution,size=(900,300)) joinpath(datadir,"GeoStatsAPI.png") !isCI
+      @test_ref_plot "data/GeoStatsAPI.png" plot(solution,size=(900,300))
     end
   end
 end
